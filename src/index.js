d1cd0af833e9fc2e825825aa82dee520fa0303d7
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-09-30 11:09:28 -0300

    refatoracao, testes passando

--- src/index.js
+++ src/index.js
@@ -19,78 +19,55 @@ const users = [];
 //localhost:3333
 
 function checksExistsUserAccount(request, response, next) {
-  /**
-   * 1.receber atraves do header o userName
-   * 2.procurar com o find() para cada user ser user.name === name e guardar o retorno numa variavel
-   * 3. se o usuario nao existir, retornar 400 e um json de erro usuario nao encontrado
-   * 4. disponibilizar o usuario encontrado para as rotas
-   * 5.return next
-   */
-  
+  //receber atraves do header o userName
   const { username } = request.headers;  
-  const userVerified = users.find(user => user.username === username);  
-  if(!userVerified) return response.status(404).json({ error: "User Not Found!"});  
-  request.userVerified = userVerified;
+  // para cada user ser user.name === name e guardar o retorno numa variavel
+  const user = users.find(user => user.username === username);  
+  //se o usuario nao existir, retornar 400 e um json de erro usuario nao encontrado
+  if(!user) {
+    return response.status(404).json({ error: "User Not Found!"});  
+  }
+  //disponibilizar o usuario encontrado para as rotas
+  request.user = user;
+  //retornar
   return next();
 }
 
 // - criar usuario
 app.post('/users', (request, response) => {
-  /**
-   * 1. receber name e userName no corpo da requisicao;
-   * 2. procurar na lista de usuarios: para cada usuario: username é igual a username recebido na requisicao?
-   * 3. se esse usuario ja existe, retorna um erro:
-   * 4. senao: construir um usuario
-   * 5. colocar no array de usuarios
-   * 6. retornar status 201 e json do usuario criado
-   */
-
+  //receber name e userName no corpo da requisicao;
   const { name, username } = request.body;
   //procurar na lista de usuarios: para cada usuario: username é igual a username recebido na requisicao?
-  const userExists = users.find(user => user.username === username);
-
+  const userExist = users.some(user => user.username === username);
   //se esse usuario ja existe, retorna um erro:
-  if(userExists) return response.status(400).json({ error: "Username Already Exists"})
-
+  if(userExist) return response.status(400).json({ error: "Username Already Exists"});
   //senao: construir um usuario
-  const user = {
+  const newUser = {
     id: uuidv4(),
     name,
     username,
     todos: []
   }
-
   //colocar no array de usuarios
-  users.push(user);
-  return response.status(201).json(user);
+  users.push(newUser);
+  //retornar status 201 e json do usuario criado
+  return response.status(201).json(newUser);
 
 });
 
 // - ler tarefas
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  /**
-   * 1. receber o usuarioExistente verificado
-   * 2. retornar a lista de tarefas deste usuario
-   */  
 
   //1. receber o usuarioExistente verificado
-  const { userVerified } = request;
+  const { user } = request;
   //2. retornar a lista de tarefas deste usuario  
-  return response.json(userVerified.todos);
+  return response.json(user.todos);
 });
 
 // - criar tarefas - criar tarefa para o usuario
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  /**
-   * 1. receber o usuario do middleware
-   * 2. receber title e deadline pelo req.body
-   * 3. tarefa no formato especificado:
-   * 4. Coloquei no array de todos a nova todo
-   * 5. armazenar no usuario.todos a tarefa
-   */
-
   //1. receber o usuario do middleware
-  const { userVerified } = request;
+  const { user } = request;
   //2. receber title e deadline pelo req.body
   const { title, deadline } = request.body;
   //3. tarefa no formato especificado:
@@ -102,95 +79,76 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
     created_at: new Date()
   }
   //4. Coloquei no array de todos a nova todo
-  userVerified.todos.push(todo);
+  user.todos.push(todo);
   //5. armazenar no usuario.todos a tarefa
   return response.status(201).json(todo);
 });
 
 // - atualizar tarefas
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  /**
-   * 1. receber o usuario do middleware
-   * 2. receber pelo corpo da req title e deadline
-   * 3. receber por params o id do todo
-   * 4. procurar nas tarefas do userVerified: em cada tarefa: se a tarefa tem o mesmo id recebido no params
-   * 5. se a tarefa nao existe, retornar o status com a mensagem de erro
-   * 6. se existe, atribuir a esta tarefa os novos valores
-   * 7, retornar a tarefa
-   */
-
-  const { userVerified } = request;
+  
+  //receber o usuario do middleware
+  const { user} = request;
+  //receber pelo corpo da req title e deadline
   const { title, deadline } = request.body;
-  const { id } = request.params;
+  //receber por params o id do todo
+  const { id: todoId } = request.params;
 
-  //4. procurar nas tarefas do userVerified: em cada tarefa: se a tarefa tem o mesmo id recebido no params
-  const updateTodo = userVerified.find(todo => todo.id === id);
+  //nao posso trocar o titulo ouo deadline de um todo que nao existe
+  // procurar no array de todos o index do todo. Se nao estiver entre 0 e arr.length, entao nao encontrou
 
-  //5. se a tarefa nao existe, retornar o status com a mensagem de erro
-  if(!updateTodo) {
-    return response.status(404).json({ error: "ToDo Not Found!"})
+  const indexOfTodo = user.todos.findIndex(todo => todo.id === todoId);
+
+  //se a tarefa nao existe, retornar o status com a mensagem de erro
+  if(indexOfTodo < 0) {
+    return response.status(404).json({ error: "Todo Not Found!"})
   }
 
-  //6. se existe, atribuir a esta tarefa os novos valores
-  updateTodo.title = title;
-  updateTodo.deadline = new Date(deadline);
+  //se existe, pego este todo
+  const todoToChange = user.todos[indexOfTodo];
+
+  //se o dado nao for undefined, atribuo o dado, senao, retorno falso
+  title ? todoToChange.title = title : false;
+  deadline ? todoToChange.deadline = deadline : false;
 
   //7. retornar a tarefa
-  return response.json(updateTodo);
+  return response.status(201).json(todoToChange);
 });
 
 // - atualizar se a tarefa foi completada pelo id
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  /**
-   * 1. receber o usuario do middleware
-   * 2. receber por params o id do todo
-   * 3. procurar nas tarefas do userVerified: em cada tarefa: se a tarefa tem o mesmo id recebido no params
-   * 4. se a tarefa nao existe, retornar o status com a mensagem de erro
-   * 5. se existe, atribuir a esta tarefa os novos valor true
-   * 6. retornar a tarefa
-   */
-
   //1. receber o usuario do middleware
-  const { userVerified } = request;
+  const { user } = request;
   //2. receber por params o id do todo
-  const { id } = request.params;
+  const { id: todoId } = request.params;
   //3. procurar nas tarefas do userVerified: em cada tarefa: se a tarefa tem o mesmo id recebido no params
-  const updateTodo = userVerified.find(todo => todo.id === id);
+  const indexOfTodo = user.todos.findIndex(todo => todo.id === todoId);
   //4. se a tarefa nao existe, retornar o status com a mensagem de erro
-  if(!updateTodo) {
-    return response.status(404).json({ error: "ToDo Not Found!"})
+  if(indexOfTodo < 0) {
+    return response.status(404).json({ error: "Todo Not Found!"})
   }
-  //5. se existe, atribuir a esta tarefa os novos valor true
-  updateTodo.done = true;
+  //5. se existe, atribuir a esta tarefa o novos valo true
+  user.todos[indexOfTodo].done = true;
   //6. retornar a tarefa
-  return response.json(updateTodo);
+  return response.status(201).json(user.todos[indexOfTodo]);
 });
 
 // - deletar uma tarefa pelo id
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-   /**
-   * 1. receber o usuario do middleware
-   * 2. receber por params o id do todo
-   * 3. retorna a posicao no array que esse objeto existe
-   * 4. se a tarefa nao existe, retornar o status com a mensagem de erro
-   * 5. retirar das todos do usuario
-   * 6. retornar a tstatus
-   */
-
+app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {   
   //1. receber o usuario do middleware
-  const { userVerified } = request;
+  const { user } = request;
   //2. receber por params o id do todo
-  const { id } = request.params;
+  const { id: todoId } = request.params;
   //3. retorna a posicao no array que esse objeto existe
-  const updateTodoIndex = userVerified.findIndex(todo => todo.id === id);
+  const indexOfTodo = user.todos.findIndex(todo => todo.id === todoId);
   //4. se o retorno for negativo, o Todo nao existe
-  if(updateTodoIndex === -1) {
-    return response.status(404).json({ error: "ToDo Not Found!"})
+  if(indexOfTodo < 0) {
+    return response.status(404).json({ error: "Todo Not Found!"})
   }
   //5. retirar das todos do usuario
-  userVerified.todos.splice(updateTodoIndex, 1);
+  user.todos.splice(indexOfTodo, 1);
   //6. retornar status
-  return response.status(204).json();
+  return response.status(204).send();
 });
 
 module.exports = app;
